1bbcac5916eb676bb3190cf8f4b185127d2eae87
Pedro Duelos <scrowzeylol@gmail.com>
2024-10-04 17:47:29 -0300

    Adicionando

--- sprint_1/Desafio/ecommerce/consolidador_de_processamento_de_vendas.sh
+++ sprint_1/Desafio/ecommerce/consolidador_de_processamento_de_vendas.sh
@@ -4,7 +4,7 @@
 
 touch /home/pedro/pb-pedro-henrique/sprint_1/relatorio_final.txt
 echo "CONTEUDO DE TODOS OS RELATORIOS:" >> /home/pedro/pb-pedro-henrique/sprint_1/relatorio_final.txt 
-cat /home/pedro/pb-pedro-henrique/sprint_1/Desafio/ecommerce/vendas/backup/*.txt >> /home/pedro/pb-pedro-henrique/sprint_1/Desafio/relatorio_final.txt
+cat /home/pedro/pb-pedro-henrique/sprint_1/Desafio/ecommerce/vendas/backup/*.txt >> /home/pedro/pb-pedro-henrique/sprint_1/Desafio/ecommerce/vendas/backup/relatorio_final.txt
 
 
 
